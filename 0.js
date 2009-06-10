7bf1907825fa6b21ff641ecdb33133ce8d9d2a40
unknown <horie88@gmail.com>
2009-06-10 18:13:58 +0900

    url change

--- 0.js
+++ 0.js
@@ -4,7 +4,7 @@ var __i=0;(function(){
 	intid=setInterval(mloop, 1000);
 	
 	function mloop(){
-		__favimg[__i].src='http://www.google.co.jp/intl/ja_jp/images/logo.gif';
+		__favimg[__i].src='http://github.com/benchan/wass/raw/124ae9ee0b36f811301188aaa9ddcfd71eb39472/batsu.png';
 		__i++;
 		if(__favimg[__i]==undefined){clearInterval(intid);}
 	}
