7c84b7a71d57e6b86be0ef9b45e42e8341d19ff6
unknown <horie88@gmail.com>
2009-06-12 12:54:05 +0900

    -

--- 1.js
+++ 1.js
@@ -1,17 +1,19 @@
-var __i=0;(function(){
+var __i=0;
+(function(){
 	__favimg=$j("div.favorite_list a img");
 	__favimg.css("position","absolute");
+	__imgs = jQuery.makeArray(__favimg);
 	__h=$j("html").height();
 	
 	intid=setInterval(mloop, 1000);
 	
 	function mloop(){
-		var _y= -__h-__favimg[__i].y;
-		__favimg[__i].animate({
+		var _y= -__h-__imgs[__i].y;
+		__imgs[__i].animate({
 			top: "-="+_y+"px"}
 			, 5000, "linear");
 		__i++;
-		if(__favimg[__i]==undefined){clearInterval(intid);}
+		if(__imgs[__i]==undefined){clearInterval(intid);}
 	}
 	
  	 
