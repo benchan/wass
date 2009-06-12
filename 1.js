33d1a2514d383b6af1609af4256b418a52e632b6
unknown <horie88@gmail.com>
2009-06-12 12:27:32 +0900

    -

---
+++ 1.js
@@ -0,0 +1,19 @@
+var __i=0;(function(){
+	__favimg=$j("div.favorite_list a img");
+	__favimg.css("position","absolute");
+	__h=$j("html").height();
+	
+	intid=setInterval(mloop, 1000);
+	
+	function mloop(){
+		var _y= -__h-__favimg[__i].y;
+		__favimg[__i].animate({
+    		left: "+=250px",
+			top: "-="+_y+"px"}
+			, 5000, "linear")
+		__i++;
+		if(__favimg[__i]==undefined){clearInterval(intid);}
+	}
+	
+ 	 
+})();
